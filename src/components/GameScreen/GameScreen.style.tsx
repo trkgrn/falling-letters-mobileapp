9797260360d42fc2fdfc45680f6d5bdb7b31b8db
trkgrn <takoyaso58@gmail.com>
2023-04-09 13:58:48 +0300

    GameScreen page designed

--- src/components/GameScreen/GameScreen.style.tsx
+++ src/components/GameScreen/GameScreen.style.tsx
@@ -1,10 +1,36 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
+import Colors from "../../styles/Colors";
+import Fonts from "../../styles/Fonts";
 
+const deviceSize = Dimensions.get("window");
 export default StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#a42424",
+    padding: 10,
+    flex: 1
+  },
+  score: {
+    color: Colors.defaultDark,
+    fontSize: 25,
+    fontFamily: Fonts.defaultFont
+  },
+  letterListContainer: {
+    backgroundColor: Colors.defaultGreen,
+    height: deviceSize.height / 1.65,
+    flexDirection: "row"
+  },
+  resultContainer: {
+    backgroundColor: Colors.defaultText,
     alignItems: "center",
+    margin: 5
+  },
+  resultText: {
+    color: Colors.defaultDark,
+    fontSize: 25,
+    fontFamily: Fonts.defaultFont
+  },
+  buttonContainer: {
+    flexDirection: "row",
     justifyContent: "center",
+    alignItems: "center"
   }
 });
--- src/components/GameScreen/GameScreen.tsx
+++ src/components/GameScreen/GameScreen.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./GameScreen.style";
 import { Text, View } from "react-native";
 import LetterCard from "../LetterCard/LetterCard";
+import Button from "../Button/Button";
 
 const GameScreen = () => {
 
+
+  const [selectedLetter, setSelectedLetter] = useState<any[]>([]);
+  const [score, setScore] = useState<number>(0);
+  const [status, setStatus] = useState<string>("ACTIVE");
+
   return (
     <View style={styles.container}>
-      <LetterCard letter={{key:1,value:"A",isClicked:false}}/>
+      <Text style={styles.score}>Score:{score} Status:{status}</Text>
+      <View style={styles.letterListContainer}>
+
+      </View>
+      <View style={styles.resultContainer}>
+        <Text style={styles.resultText}></Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title={"OK"} ></Button>
+        <Button title={"Reset"} ></Button>
+      </View>
     </View>
   );
 }
